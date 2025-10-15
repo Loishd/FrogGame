f2463d00bc905b8974bec9137d0d4c5c545723a9
win2018hero <chayut.winner@gmail.com>
2025-10-16 00:44:41 +0700

    V0.0.95
    +Add Music system & SFX system (processing)
    +Add button ui system

---
+++ Assets/Scripts/CustomButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomButton : MonoBehaviour
+{
+    private void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
