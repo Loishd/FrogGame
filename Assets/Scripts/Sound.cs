f462c0f36aa502c3ff045efb919883594ad6bd31
win2018hero <chayut.winner@gmail.com>
2025-10-22 17:04:11 +0700

    V0.1.205
    +BGM system

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Sound : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     void Start()
     {
         
@@ -14,7 +14,7 @@ public class Test : MonoBehaviour
         SoundManager.Instance.PlaySound2D("group1");
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
